3731c30371ec4fad0f44eb9b9130fc58494df8ad
namnamnamnam <tranducnam7@gmail.com>
2018-03-24 16:14:30 +0700

    Update plugin_highcharts.js

--- plugin_highcharts.js
+++ plugin_highcharts.js
@@ -443,7 +443,8 @@
 					var x = (new Date()).getTime();
 					// console.log('addPoint:', x,currentSettings[seriesno], Number(newValue));
 					var plotMqtt = [x, Number(newValue)]; //create the array+ "Y"
-					//series.addPoint(plotMqtt, true, shift);
+					series.addPoint(plotMqtt, true, shift);
+					series.addPoint(plotMqtt, true, shift);
 				};
 			}
 		}
